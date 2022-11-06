8dc74d4707d8bbd7800bfabf3fbe14b06f82dc62
trsnaqe <trsnaqe@pop-os.localdomain>
2022-11-06 05:18:53 +0300

    user offline, map removal, disconnect, dupliacte name checker

--- client.cxx
+++ client.cxx
@@ -11,7 +11,6 @@
 #include <vector>
 #include <csignal>
 
-
 using namespace std;
 
 int n, privatepipe;
@@ -19,36 +18,9 @@ char selfpipe_path[50];
 static char buffer[PIPE_BUF];
 struct message msg;
 int publicpipe;
-
-void on_messsage(int signum)
-{
-    if ((privatepipe = open(selfpipe_path, O_RDONLY)) == -1)
-    {
-        perror(selfpipe_path);
-        return;
-    }
-
-
-    while ((n = read(privatepipe, buffer, PIPE_BUF)) > 0)
-    {
-        write(fileno(stdout), buffer, n);
-    }
-    cout << endl;
-
-    close(privatepipe);
-}
-
-void handle_target()
-{
-    // TODO: daha cok herkesin kendi room'u gibi yapalim
-    // degilse, ahmet dedikten sonra da araya mehmet girebiliyor
-    
-    string target_user;
-    cout << "\nWho do you want to send message?\n"
-         << ">";
-    cin >> target_user;
-    sprintf(msg.target_user, "%s", target_user.c_str());
-}
+string from_user;
+string target_user;
+string ERRORS[] = {"SERVER_DUPL_NAME_ERROR", "SERVER_TARGET_NOT_FOUND_ERROR"};
 
 void server_init()
 {
@@ -57,13 +29,78 @@ void server_init()
     sprintf(msg.content, "%d", getpid());
     write(publicpipe, (char *)&msg, sizeof(msg));
 }
+void handle_target(int signum = 15)
+{
+    // TODO: daha cok herkesin kendi room'u gibi yapalim
+    // degilse, ahmet dedikten sonra da araya mehmet girebiliyor
+
+    cout << "\nWho do you want to send message?\n"
+         << ">";
+    cin >> target_user;
+    cout << "new target user name is " << target_user << "\n";
+    sprintf(msg.target_user, "%s", target_user.c_str());
+}
+void handle_duplicate_name()
+{
+    string tempName;
+    tempName.assign(from_user);
+    while (!tempName.compare(from_user))
+    {
+        cout << "This name has already been taken please choose new name\n";
+        cout << "Your new name is:?\n"
+             << ">";
+        cin >> from_user;
+    }
+    sprintf(msg.from_user, "%s", from_user.c_str());
+    server_init();
+    cout << "Waiting for server to verify your name...";
+    pause();
+}
+void on_messsage(int signum)
+{
+    if ((privatepipe = open(selfpipe_path, O_RDONLY)) == -1)
+    {
+        perror(selfpipe_path);
+        return;
+    }
+
+    while ((n = read(privatepipe, buffer, PIPE_BUF)) > 0)
+    {
+        if (buffer == ERRORS[0])
+        {
+            handle_duplicate_name();
+        }
+        else
+            write(fileno(stdout), buffer, n);
+    }
+    cout << endl;
+
+    close(privatepipe);
+}
+void handle_offline(int signum)
+{
+    cout << "User is offline! Please try again later!\n";
+    handle_target();
+}
+void handle_disconnect(int signum)
+{
+    sprintf(msg.target_user, "USER-DISCONNECT");
+    sprintf(msg.content, "%d", getpid());
+    write(publicpipe, (char *)&msg, sizeof(msg));
+    cout<<"Successfully disconnected";
+    exit(0);
+}
+
 
 int main()
 {
+    signal(2, handle_disconnect);
+    signal(4, handle_offline);
     signal(10, on_messsage);
+    signal(15, handle_target);
 
-    string from_user;
-    cout << "Hi, What's your name?\n" << ">";
+    cout << "Hi, What's your name?\n"
+         << ">";
     cin >> from_user;
 
     sprintf(msg.from_user, "%s", from_user.c_str());
@@ -83,17 +120,16 @@ int main()
         return 2;
     }
     // write(fileno(stdout), "code working so far3", 20);
-
     server_init();
-
-    handle_target();
+    cout << "Waiting for server to verify your name...\n";
+    pause();
 
     while (1)
-    { 
+    {
+
         memset(msg.content, 0x0, B_SIZ);
 
         n = read(fileno(stdin), msg.content, B_SIZ);
-
 
         if (!strcmp("<-\n", msg.content))
         {
--- server.cxx
+++ server.cxx
@@ -7,10 +7,10 @@
 using namespace std;
 
 struct message msg;
+struct message serverMessage;
 map<string, int> user_pid;
 char targetpipe_path[50];
-
-
+string ERRORS[] = {"SERVER_DUPL_NAME_ERROR", "SERVER_TARGET_NOT_FOUND_ERROR"};
 int number_parser(char arr[], int size)
 {
     int j = 0;
@@ -45,12 +45,67 @@ void user_init()
     // char[] to string and map insertion
     string from(msg.from_user);
     user_pid.insert(pair<string, int>(from, pid));
-
-
+    kill(pid, 15);
     cout << "New User<" << from << "," << user_pid[from] << "> saved!" << endl;
 }
 
+void handle_send_error(int targetPid)
+{
+    kill(targetPid, 10);
+    sprintf(targetpipe_path, "/tmp/pipe%d", targetPid);
+    int targetPrivatePipe;
+    int done = 0;
+    int n = 0;
+    do
+    {
+        if ((targetPrivatePipe = open(targetpipe_path, O_WRONLY | O_NDELAY)) == -1)
+        {
+            sleep(3);
+        }
+        else
+        {
 
+            write(targetPrivatePipe, serverMessage.content, B_SIZ);
+            close(targetPrivatePipe);
+            done = 1;
+        }
+    } while (++n < 5 && !done);
+    if (!done)
+    {
+        write(fileno(stderr),
+              "\nNOTE: SERVER ** NEVER ** accessed private FIFO\n", 48);
+    }
+}
+void error_handler(int errorType, int targetPid)
+{
+
+    sprintf(serverMessage.from_user, "%s", "SERVER");
+    sprintf(serverMessage.content, "%s", ERRORS[errorType].c_str());
+    sprintf(serverMessage.target_user, "%d", targetPid);
+    handle_send_error(targetPid);
+}
+
+void handle_taken_name()
+{
+    // pid of faulty user
+    int targetPid;
+    sscanf(msg.content, "%d", &targetPid);
+    error_handler(0, targetPid);
+}
+void handle_user_disconnect()
+{
+    printf("BEFOREEE");
+    for (auto it = user_pid.cbegin(); it != user_pid.cend(); ++it)
+    {
+        std::cout << "{" << (*it).first << ": " << (*it).second << "}\n";
+    }
+    user_pid.erase(msg.from_user);
+    printf("AFTERRR");
+    for (auto it = user_pid.cbegin(); it != user_pid.cend(); ++it)
+    {
+        std::cout << "{" << (*it).first << ": " << (*it).second << "}\n";
+    }
+}
 int main()
 {
     int n, done, dummypipe, publicpipe, privatepipe;
@@ -70,42 +125,71 @@ int main()
         cout << "From: " << msg.from_user << "\nTo: " << msg.target_user << "\nContent: " << msg.content << endl;
         if (!strcmp(msg.target_user, "SERVER-INIT"))
         {
-            user_init();
-            //TODO: send error to sender if name is used
-            //client should re-take server_init action
-            continue;
-        }
-        n = done = 0;
-
-
-        string target(msg.target_user);
-        int target_pid = user_pid[target];
-        if (!target_pid) {
-            // TODO: send to sender:
-            // SERVER: Target user is offline!
-            continue;
-        }
-
-        kill(target_pid, 10);
-        sprintf(targetpipe_path, "/tmp/pipe%d", target_pid);
-        cout << "Target pipe path: " << targetpipe_path << endl;
-
-        do {
-            if ((privatepipe = open(targetpipe_path, O_WRONLY | O_NDELAY)) == -1) {
-                sleep(3);
-            } else {
-                //TODO, su anda client sadece msg.content aliyor
-                // tum mesaj objesini gonder.
-                write(privatepipe, msg.content, B_SIZ);
-                close(privatepipe);
-                done = 1;
+            // checks if users map has username!
+            // unless it's multidimensional map, map.count(key) returns 1 if it contains the key.
+            bool isUserInMap = user_pid.count(msg.from_user) == 1 ? true : false;
+            if (isUserInMap)
+            {
+                handle_taken_name();
+                cout << "This map has user" << msg.from_user << endl;
+                cout << "This map has user with pid" << msg.content << endl;
             }
-        } while (++n < 5 && !done);
-        if (!done)
+            // shouldnt init if it has in map
+            else
+                user_init();
+            // TODO: send error to sender if name is used
+            // client should re-take server_init action
+            continue;
+        }
+        else if (!strcmp(msg.target_user, "USER-DISCONNECT"))
         {
-            write(fileno(stderr),
-                "\nNOTE: SERVER ** NEVER ** accessed private FIFO\n", 48);
-            return 2;
+            bool isUserInMap = user_pid.count(msg.from_user) == 1 ? true : false;
+            if (isUserInMap)
+            {
+                handle_user_disconnect();
+            }
+
+            continue;
+        }
+        else
+        {
+            n = done = 0;
+            string target(msg.target_user);
+            int target_pid = user_pid[target];
+            if (!target_pid)
+            {
+                    kill(user_pid[msg.from_user], 4);
+                // TODO: send to sender:
+                // SERVER: Target user is offline!
+                continue;
+            }
+
+            if (target_pid != 0)
+                kill(target_pid, 10);
+            sprintf(targetpipe_path, "/tmp/pipe%d", target_pid);
+            cout << "Target pipe path: " << targetpipe_path << endl;
+
+            do
+            {
+                if ((privatepipe = open(targetpipe_path, O_WRONLY | O_NDELAY)) == -1)
+                {
+                    sleep(3);
+                }
+                else
+                {
+                    // TODO, su anda client sadece msg.content aliyor
+                    //  tum mesaj objesini gonder.
+                    write(privatepipe, msg.content, B_SIZ);
+                    close(privatepipe);
+                    done = 1;
+                }
+            } while (++n < 5 && !done);
+            if (!done)
+            {
+                write(fileno(stderr),
+                      "\nNOTE: SERVER ** NEVER ** accessed private FIFO\n", 48);
+                return 2;
+            }
         }
     }
     return 0;
