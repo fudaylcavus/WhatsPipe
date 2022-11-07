90641ffddbc251ead1715f202707ef22efb8be62
Fudayl Çavuş <fudaylcavus@gmai.com>
2022-11-07 20:15:38 +0300

    memset correct usage

--- local.h
+++ local.h
@@ -3,12 +3,12 @@
 #include <sys/stat.h> 
 #include <fcntl.h> 
 #include <unistd.h> 
-#include <string.h>
 #include <stdlib.h>
 using namespace std;
 const int PIPE_BUF = 8192;
 const char *PUBLIC = "/tmp/PUBLIC"; 
 const int B_SIZ = (PIPE_BUF / 2);
+
 struct message {
     char target_user[50]; 
     char content[PIPE_BUF - 100];
