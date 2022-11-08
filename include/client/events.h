4673f91a0835ee9ce9f00695ffc315b1843478d2
Fudayl Çavuş <fudaylcavus@gmai.com>
2022-11-08 18:35:53 +0300

    Client Modularization

---
+++ include/client/events.h
@@ -0,0 +1,10 @@
+#include <string>
+
+
+void on_disconnect(int);
+void on_message(int);
+void on_serverinit();
+void on_duplicatename();
+void on_winresize(int);
+void on_newtarget(int);
+void on_useroffline(int);
